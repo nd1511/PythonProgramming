cc0f81f271a5426d8b06ab43707f33d5ec82323c
nd1511 <nd1511@imperial.ac.uk>
2018-05-02 15:02:51 +0100

    Python, PyCharm IDE
    Python, PyCharm IDE

---
+++ program19_CNN.py
@@ -0,0 +1,167 @@
+
+from keras import layers
+from keras import models
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+
+input_shape=(150, 150, 3)))
+
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+
+model.add(layers.Dense(512, activation='relu'))
+
+model.add(layers.Dense(1, activation='sigmoid'))
+
+#model.summary()
+#print(model.summary())
+
+
+
+from keras import optimizers
+
+model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+
+#model.summary()
+print(model.summary())
+
+
+
+# we now use dropout
+
+# we use dropout
+model = models.Sequential()
+
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Flatten())
+
+model.add(layers.Dropout(0.5))
+
+model.add(layers.Dense(512, activation='relu'))
+
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+
+#model.summary()
+print(model.summary())
+
+
+
+import os, shutil
+
+original_dataset_dir = '/Users/dionelisnikolaos/Downloads/kaggle_original_data'
+
+base_dir = '/Users/dionelisnikolaos/Downloads/cats_and_dogs_small'
+os.mkdir(base_dir)
+
+train_dir = os.path.join(base_dir, 'train')
+os.mkdir(train_dir)
+
+validation_dir = os.path.join(base_dir, 'validation')
+os.mkdir(validation_dir)
+
+test_dir = os.path.join(base_dir, 'test')
+os.mkdir(test_dir)
+
+train_cats_dir = os.path.join(train_dir, 'cats')
+os.mkdir(train_cats_dir)
+
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+os.mkdir(train_dogs_dir)
+
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+os.mkdir(validation_cats_dir)
+
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+os.mkdir(validation_dogs_dir)
+
+test_cats_dir = os.path.join(test_dir, 'cats')
+os.mkdir(test_cats_dir)
+
+test_dogs_dir = os.path.join(test_dir, 'dogs')
+os.mkdir(test_dogs_dir)
+
+
+
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
+
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(train_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
+
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(validation_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
+
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(test_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
+
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(train_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+
+fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
+
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(validation_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+
+fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
+
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(test_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+
+
+
+print('total training cat images:', len(os.listdir(train_cats_dir)))
+
+print('total training dog images:', len(os.listdir(train_dogs_dir)))
+
+print('total validation cat images:', len(os.listdir(validation_cats_dir)))
+
+print('total validation dog images:', len(os.listdir(validation_dogs_dir)))
+
+print('total test cat images:', len(os.listdir(test_cats_dir)))
+
+print('total test dog images:', len(os.listdir(test_dogs_dir)))
+
+
+
+
+
