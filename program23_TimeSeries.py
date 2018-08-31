e4844123ba40b1c551e1e977ed3c103a4d06c3b7
nd1511 <nd1511@imperial.ac.uk>
2018-09-01 00:17:33 +0300

    Python, PyCharm IDE
    Python, PyCharm IDE

--- program23_TimeSeries.py
+++ program23_TimeSeries.py
@@ -320,6 +320,404 @@ df = pd.read_csv('/Users/dionelisnikolaos/Downloads/dataset_cryptocurrency.csv')
 # output first 5 rows in dataset
 print(df.head())
 
+# output first 9 rows in dataset
+print(df.head(9))
+
+# The cryptocurrency dataset contains data for 4 cryptocurrencies (Bitcoin, Ethereum, Ripple, Bitcoin-Cash)
+# over a 5 year period 2013-2018. The provided data is all in $ USD and includes open/close, high/low, volume traded, spread and close ratio.
+# The close ratio is defined as: Close Ratio = (Close - Low)/(High - Low)
+
+# we compute the close ration using Close Ratio = (Close - Low)/(High - Low)
+
+# In the Excel file: Bitcoin (BTC) is from 2 to 1867
+# BTC is from 2 to 1867
+# ETH is from 1868 to 2902
+# XRP is from 2903 to 4670
+# BCH is from 4671 to 4989
+
+# In the stored pandas array
+# BTC is from 0 to 1865
+# ETH is from 1866 to 2900
+# XRP is from 2901 to 4668
+# BCH is from 4669 to 4987
+
+# output first 1865+1 rows in dataset
+#print(df.head(1865+1))
+# this has all BTC
+
+# output first 1865+1+1 rows in dataset
+#print(df.head(1865+1+1))
+# this has all BTC except the last one
+
+#BTC = df.head(1865+1)
+#print(BTC)
+#print(BTC.size)
+
+#BTC = df[0:1865]
+BTC = df[0:1865+1]
+#print(BTC)
+
+#ETH = df.head(2900+1)
+#ETH = df.DataFrame({'symbol':'ETH'})
+#print(ETH)
+
+#ETH = df[1866:2900]
+ETH = df[1866:2900+1]
+#print(ETH)
+
+#XRP = df.head(4668+1)
+#XRP = df[2901:4668]
+XRP = df[2901:4668+1]
+#print(XRP)
+
+#BCH = df.head(4987+1)
+#BCH = df.tail(4987-4669)
+#print(BCH)
+#print(BCH.size)
+
+#BCH = df[4669:4987]
+BCH = df[4669:4987+1]
+#print(BCH)
+
+BCH_close_ratio = BCH.close_ratio
+#print(BCH_close_ratio)
+
+XRP_close_ratio = XRP.close_ratio
+#print(XRP_close_ratio)
+
+ETH_close_ratio = ETH.close_ratio
+#print(ETH_close_ratio)
+
+BTC_close_ratio = BTC.close_ratio
+#print(BTC_close_ratio)
+
+
+
+# we now plot the Close Ratios
+
+# we plot BTC
+plt.figure()
+
+#plt.plot(BTC_close_ratio, 'bo-', label='BTC close_ratio')
+#plt.plot(ETH_close_ratio, 'ro-', label='ETH close_ratio')
+#plt.plot(XRP_close_ratio, 'ko-', label='XRP close_ratio')
+#plt.plot(BCH_close_ratio, 'mo-', label='BCH close_ratio')
+
+plt.plot(range(0, 100+1), BTC_close_ratio[0:100+1], 'bo-', label='BTC close_ratio')
+
+#plt.plot(BTC_close_ratio[0:100+1], 'bo-', label='BTC close_ratio')
+#plt.plot(ETH_close_ratio[0:100+1], 'ro-', label='ETH close_ratio')
+#plt.plot(XRP_close_ratio[0:100+1], 'ko-', label='XRP close_ratio')
+#plt.plot(BCH_close_ratio[0:100+1], 'mo-', label='BCH close_ratio')
+
+plt.title('The close ratios.')
+plt.legend()
+
+plt.show()
+
+# we plot ETH
+plt.figure()
+
+plt.plot(range(0, 100+1), ETH_close_ratio[0:100+1], 'ro-', label='ETH close_ratio')
+
+plt.title('The close ratios.')
+plt.legend()
+
+plt.show()
+
+# we plot all together
+plt.figure()
+
+plt.plot(range(0, 100+1), BTC_close_ratio[0:100+1], 'bo-', label='BTC close_ratio')
+plt.plot(range(0, 100+1), ETH_close_ratio[0:100+1], 'ro-', label='ETH close_ratio')
+plt.plot(range(0, 100+1), XRP_close_ratio[0:100+1], 'ko-', label='XRP close_ratio')
+plt.plot(range(0, 100+1), BCH_close_ratio[0:100+1], 'mo-', label='BCH close_ratio')
+
+plt.title('The close ratios.')
+plt.legend()
+
+plt.show()
+
+
+
+# we compute the auto-correlation coefficients
+
+# we compute the auto-correlation coefficients for BTC
+
+#x = (?)
+#x = array1
+x = BTC_close_ratio
+
+#print(array1.size)
+#N = x.size
+
+#N = x.size
+N = len(x)
+#print(N)
+
+#M = N / 10
+#print(M)
+
+import math
+M = math.floor(N / 10)
+#print(M)
+
+#c_k = np.zeros(int(M))
+c_k = np.zeros(M)
+
+x_bar = 0
+
+#print(x[0,0])
+#print(x[0,1])
+
+for t in range(1-1,N-1):
+    x_bar = x_bar + (x[t])
+
+x_bar = x_bar / N
+
+for m in range(1-1, M):
+    for t in range(1-1, N-m):
+        c_k[m] = c_k[m] + ((x[t] - x_bar) * (x[t+m] - x_bar))
+
+c_k = (1/N) * c_k
+
+r_k = c_k / c_k[0]
+
+#print(c_k)
+#print(c_k.size)
+
+#print(c_k.size)
+#print(r_k.size)
+
+# we find the auto-correlation coefficients
+# we compute the auto-correlation function, the auto-correlation coefficients, the correlogram
+
+plt.figure()
+
+#plt.plot(r_k, 'bo-', label='auto-correlation coefficients')
+plt.plot(range(1,M+1), r_k, 'bo-', label='BTC auto-correlation coefficients')
+#plt.plot((1:len(x))*(1/len(x))*21, x, 'bo-', label='Yield')
+
+plt.title('The auto-correlation coefficients, the correlogram, for BTC.')
+plt.legend()
+
+plt.show()
+
+# we see that the auto-correlation coefficients go to zero
+
+# the auto-correlation coefficients come down quickly towards zero
+# from the plot, we observe that the auto-correlation coefficients go to zero fast
+
+
+
+# we compute the auto-correlation coefficients for ETH
+
+#x = (?)
+#x = array1
+x = ETH_close_ratio
+
+x = np.asarray(x)
+
+#print(array1.size)
+#N = x.size
+
+#N = x.size
+N = len(x)
+#print(N)
+
+#M = N / 10
+#print(M)
+
+import math
+M = math.floor(N / 10)
+#print(M)
+
+#c_k = np.zeros(int(M))
+c_k = np.zeros(M)
+
+x_bar = 0
+
+#print(x[0,0])
+#print(x[0,1])
+
+for t in range(1-1,N-1):
+    x_bar = x_bar + (x[t])
+
+x_bar = x_bar / N
+
+for m in range(1-1, M):
+    for t in range(1-1, N-m):
+        c_k[m] = c_k[m] + ((x[t] - x_bar) * (x[t+m] - x_bar))
+
+c_k = (1/N) * c_k
+
+r_k = c_k / c_k[0]
+
+#print(c_k)
+#print(c_k.size)
+
+#print(c_k.size)
+#print(r_k.size)
+
+# we find the auto-correlation coefficients
+# we compute the auto-correlation function, the auto-correlation coefficients, the correlogram
+
+plt.figure()
+
+#plt.plot(r_k, 'bo-', label='auto-correlation coefficients')
+plt.plot(range(1,M+1), r_k, 'ro-', label='ETH auto-correlation coefficients')
+#plt.plot((1:len(x))*(1/len(x))*21, x, 'bo-', label='Yield')
+
+plt.title('The auto-correlation coefficients, the correlogram, for ETH.')
+plt.legend()
+
+plt.show()
+
+# we see that the auto-correlation coefficients go to zero
+
+# the auto-correlation coefficients come down quickly towards zero
+# from the plot, we observe that the auto-correlation coefficients go to zero fast
+
+
+
+# we compute the auto-correlation coefficients for XRP
+
+#x = (?)
+#x = array1
+x = XRP_close_ratio
+
+x = np.asarray(x)
+
+#print(array1.size)
+#N = x.size
+
+#N = x.size
+N = len(x)
+#print(N)
+
+#M = N / 10
+#print(M)
+
+import math
+M = math.floor(N / 10)
+#print(M)
+
+#c_k = np.zeros(int(M))
+c_k = np.zeros(M)
+
+x_bar = 0
+
+#print(x[0,0])
+#print(x[0,1])
+
+for t in range(1-1,N-1):
+    x_bar = x_bar + (x[t])
+
+x_bar = x_bar / N
+
+for m in range(1-1, M):
+    for t in range(1-1, N-m):
+        c_k[m] = c_k[m] + ((x[t] - x_bar) * (x[t+m] - x_bar))
+
+c_k = (1/N) * c_k
+
+r_k = c_k / c_k[0]
+
+#print(c_k)
+#print(c_k.size)
+
+#print(c_k.size)
+#print(r_k.size)
+
+# we find the auto-correlation coefficients
+# we compute the auto-correlation function, the auto-correlation coefficients, the correlogram
+
+plt.figure()
+
+#plt.plot(r_k, 'bo-', label='auto-correlation coefficients')
+plt.plot(range(1,M+1), r_k, 'ko-', label='XRP auto-correlation coefficients')
+#plt.plot((1:len(x))*(1/len(x))*21, x, 'bo-', label='Yield')
+
+plt.title('The auto-correlation coefficients, the correlogram, for XRP.')
+plt.legend()
+
+plt.show()
+
+# we see that the auto-correlation coefficients go to zero
+
+# the auto-correlation coefficients come down quickly towards zero
+# from the plot, we observe that the auto-correlation coefficients go to zero fast
+
+
+
+# we compute the auto-correlation coefficients for BCH
+
+#x = (?)
+#x = array1
+x = BCH_close_ratio
+
+x = np.asarray(x)
+
+#print(array1.size)
+#N = x.size
+
+#N = x.size
+N = len(x)
+#print(N)
+
+#M = N / 10
+#print(M)
+
+import math
+M = math.floor(N / 10)
+#print(M)
+
+#c_k = np.zeros(int(M))
+c_k = np.zeros(M)
+
+x_bar = 0
+
+#print(x[0,0])
+#print(x[0,1])
+
+for t in range(1-1,N-1):
+    x_bar = x_bar + (x[t])
+
+x_bar = x_bar / N
+
+for m in range(1-1, M):
+    for t in range(1-1, N-m):
+        c_k[m] = c_k[m] + ((x[t] - x_bar) * (x[t+m] - x_bar))
+
+c_k = (1/N) * c_k
+
+r_k = c_k / c_k[0]
+
+#print(c_k)
+#print(c_k.size)
+
+#print(c_k.size)
+#print(r_k.size)
+
+# we find the auto-correlation coefficients
+# we compute the auto-correlation function, the auto-correlation coefficients, the correlogram
+
+plt.figure()
+
+#plt.plot(r_k, 'bo-', label='auto-correlation coefficients')
+plt.plot(range(1,M+1), r_k, 'mo-', label='BCH auto-correlation coefficients')
+#plt.plot((1:len(x))*(1/len(x))*21, x, 'bo-', label='Yield')
+
+plt.title('The auto-correlation coefficients, the correlogram, for BCH.')
+plt.legend()
+
+plt.show()
+
+# we see that the auto-correlation coefficients go to zero
+
+# the auto-correlation coefficients come down quickly towards zero
+# from the plot, we observe that the auto-correlation coefficients go to zero fast
+
 
 
 
@@ -349,6 +747,8 @@ print(test_images.shape)
 
 print(len(test_labels))
 print(test_labels)
+
+
 
 
 
