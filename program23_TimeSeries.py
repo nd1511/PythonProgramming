9dbc431bf8db4546d38e0a5abb27003f39b7243d
nd1511 <nd1511@imperial.ac.uk>
2018-12-19 20:06:34 +0200

    Python, PyCharm IDE
    Python, PyCharm IDE

--- program23_TimeSeries.py
+++ program23_TimeSeries.py
@@ -26,6 +26,170 @@ import math
 
 #quandl.ApiConfig.api_key = "bA8kfL-FkszhWuXM18Pe" #setting up your api-key
 
+import numpy
+import pandas
+
+#help(list)
+# use "help(list)" to read description
+
+list1 = []
+
+# from -100 to 100
+#for i in range(-100, 100+1, 2):
+#    list1.append(i)
+
+# from -90 to 100
+for i in range(-90, 100+1, 2):
+    list1.append(i)
+
+print(list1)
+print("")
+
+# the first: print(list1[0])
+# the last: print(list1[-1])
+
+print(list1[0])
+print(list1[-1])
+print("")
+
+print(list1[::1])
+print("")
+
+print(list1[::2])
+print("")
+
+# reverse the list
+print(list1[::-1])
+print("")
+
+# reverse the list
+#print([list1[-1:0:-1], list1[0]])
+#print([list1[-1:0:-1], [list1[0]]])
+print(list1[-1:0:-1] + [list1[0]])
+print("")
+
+print(list1[::-2])
+print("")
+
+#help(list)
+
+# Kaggle competitions
+# https://www.kaggle.com/nd1511
+# Hackathons and Kaggle
+
+list2 = ['Nikolaos', 23]
+
+print(list2[0])
+print(list2[1])
+print("")
+
+#print(list2[2])
+#print("")
+
+list2 = [23, 'hello', 145]
+
+print(list2[0])
+print(list2[1])
+print(list2[2])
+print("")
+
+#print(list2[3])
+#print("")
+
+print(list2[1][0])
+print(list2[1][1])
+print("")
+
+# we use: in range(1, n+1, 1)
+# use: in range(start, end+1, step size)
+
+# we use for loop
+# for i in range(start, end+1, step size)
+
+# if statements
+# if else statements
+# for loops
+
+# factorial, !
+# 0! = 1, 1! = 1
+
+n = int(input("Give a Number: "))
+
+p = 1
+
+# use: range(1, n+1, 1)
+for i in range(1, n+1, 1):
+    p = p * i
+
+#print(p)
+print(n, "! = ", p)
+
+# dry run
+# dry run the code
+
+# run the code like the interpreter
+
+# range(startValue, endValue+1, stepSize)
+# range(1,x+1,1): until x only
+
+# recursion, we use return
+# function, def, return
+
+# base case to end recursion
+
+# functions: use def and return
+def factorial(x):
+    p = 1
+
+    # use: range(StartValue, EndValue+1, StepSize)
+    for i in range(1, x+1, 1):
+        p = p * i
+
+    print(x, "! = ", p)
+
+factorial(n)
+
+# recursive implementations take more memory and are slower
+# recursive implementations are neater
+
+# recursive implementations are more understandable
+
+"""
+factorial
+n! = 1*2*3*..*n, where 1! = 1, 0! = 1
+3! = 1 * 2 * 3 = 6
+"""
+
+print("")
+
+def factorial(x):
+    #print("I am in the factorial function.")
+    p = 1
+
+    for i in range(1,x+1,1):
+        p = p*i
+
+    print(n,"! = ",p)
+
+# Recursive: The function calls itself
+def recfactorial(x):
+    #print("I am in the recfactorial function.")
+    if x == 1:
+       return 1
+    else:
+       return x * recfactorial(x-1)
+
+# main program
+#n = int(input("Give me a number "))
+
+factorial(n)
+
+print(n,"! = ", recfactorial(n), ' with recursive')
+print("")
+
+print("I am back in the main program.")
+print("")
+
 # we use Chris Chatfield's book
 # we use: The Analysis of Time Series: An Introduction, Chris Chatfield, 6th edition (2004), Chapman & Hall / CRC.
 
