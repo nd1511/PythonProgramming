0d7aff433b2701de2a4ecf0ed22cf1a78d346866
nd1511 <nd1511@imperial.ac.uk>
2019-01-06 19:08:01 +0200

    Python, PyCharm IDE
    Python, PyCharm IDE

--- program22_LSTM_RNN.py
+++ program22_LSTM_RNN.py
@@ -1,4 +1,3 @@
-
 # we use: F. Chollet's book "Deep learning with Python"
 
 # we use: http://www.deeplearningitalia.com/wp-content/uploads/2017/12/Dropbox_Chollet.pdf
@@ -6,6 +5,7 @@
 
 # LSTM = Long Short Term memory, LSTM recurrent neural networks (RNNs)
 # GRU = Gated Recurrent Unit, GRU RNNs
+
 # we use: LSTM RNNs and GRU RNNs
 
 # LSTM RNNs are used in (https://research.google.com/pubs/archive/44312.pdf)
@@ -57,6 +57,7 @@ print(len(lines))
 # '"sh (g/kg)"', '"H2OC (mmol/mol)"', '"rho (g/m**3)"', '"wv (m/s)"', '"max. wv (m/s)"', '"wd (deg)"']
 # 420551
 
+# use numpy
 import numpy as np
 
 float_data = np.zeros((len(lines), len(header) - 1))
@@ -70,6 +71,7 @@ for i, line in enumerate(lines):
 
 
 
+# use pyplot
 from matplotlib import pyplot as plt
 
 temp = float_data[:, 1]
@@ -89,8 +91,6 @@ float_data -= mean
 
 std = float_data[:200000].std(axis=0)
 float_data /= std
-
-
 
 def generator(data, lookback, delay, min_index, max_index, shuffle=False, batch_size=128, step=6):
     if max_index is None:
@@ -263,9 +263,6 @@ history = model.fit_generator(train_gen, steps_per_epoch=500, epochs=40, validat
 
 
 
-
-
-
 # use bidirectional RNNs
 
 # use a different model
@@ -342,10 +339,363 @@ model.add(layers.Dense(1))
 
 model.compile(optimizer=RMSprop(), loss='mae')
 
-history = model.fit_generator(train_gen, steps_per_epoch=500, epochs=40, validation_data=val_gen, validation_steps=val_steps)
+history = model.fit_generator(train_gen, steps_per_epoch=500, epochs=40, \
+                              validation_data=val_gen, validation_steps=val_steps)
 
 
 
+# use: http://interactivepython.org/runestone/static/pythonds/index.html#
+
+# binary search => requires a sorted list
+# we use: http://interactivepython.org/runestone/static/pythonds/Recursion/toctree.html
+
+# binary search
+def binarySearch(list1, item1):
+    upper = len(list1) - 1
+    lower = 0
+
+    # define Boolean found
+    found = False
+
+    while not found and upper >= lower:
+        mid = (upper + lower) // 2
+
+        if list1[mid] == item1:
+            found = True
+        elif list1[mid] < item1:
+            lower = mid + 1
+        else:
+            upper = mid - 1
+
+        # if upper < lower:
+        #    break
+
+    # return the Boolean found
+    return found
+
+list1 = [4, 5, 6, 7]
+print(binarySearch(list1, 6))
+
+# def binarySearch_rec(list1, item1, upper=len(list1)-1, lower=0):
+def binarySearch_rec(list1, item1, upper, lower=0):
+    mid = (upper + lower) // 2
+
+    if upper < lower:
+        return False
+
+    if item1 == list1[mid]:
+        return True
+
+    return binarySearch_rec(list1, item1, upper, mid + 1) if item1 > list1[mid] \
+        else binarySearch_rec(list1, item1, mid - 1, lower)
+
+list1 = [4, 5, 6, 7]
+print(binarySearch_rec(list1, 6, len(list1) - 1))
+
+print(binarySearch_rec(list1, 3, len(list1) - 1))
+print('')
+
+# sorted list
+# binary search needs a sorted list
+
+list2 = [6, 7, 6, 7, 4, 5, 6, 7, 6, 7, 1, -1, 1, 2, 0, -1, 1]
+list2.sort()
+
+print(binarySearch_rec(list2, -1, len(list2) - 1))
+print(binarySearch_rec(list2, 2, len(list2) - 1))
+
+print(binarySearch_rec(list2, 3, len(list2) - 1))
+print('')
 
 
+
+# memoization
+# Fibonacci and memoization
+
+# we use: https://www.youtube.com/watch?v=Qk0zUZW-U_M
+
+# use: http://interactivepython.org/runestone/static/pythonds/index.html#
+# http://interactivepython.org/runestone/static/pythonds/Recursion/toctree.html
+
+# Fibonacci series with recursion
+def Fib_rec(n):
+    if n == 0:
+        return 1
+    if n == 1:
+        return 1
+    return Fib_rec(n - 1) + Fib_rec(n - 2)
+
+print(Fib_rec(1))
+print(Fib_rec(2))
+
+print(Fib_rec(3))
+print(Fib_rec(4))
+
+print(Fib_rec(10))
+print('')
+
+# Fibonacci series with no recursion
+def Fib(n):
+    prev = 1
+    last = 1
+
+    for i in range(1, n):
+        # prev = last
+        # last = prev + last
+
+        last, prev = (prev + last), last
+
+    return last
+
+print(Fib(1))
+print(Fib(2))
+
+print(Fib(3))
+print(Fib(4))
+
+print(Fib(10))
+print('')
+
+# website: http://interactivepython.org/runestone/static/pythonds/index.html#
+
+# no recursion
+def sumFib(n):
+    prev = 1
+    last = 1
+    sum1 = 2
+
+    if n == 0:
+        return 1
+
+    for i in range(1, n):
+        # prev = last
+        last, prev = (prev + last), last
+
+        sum1 += last
+
+    return sum1
+
+print(sumFib(3))
+print(sumFib(4))
+
+print(sumFib(10))
+print('')
+
+# recursion
+def sumFib_rec(n):
+    if n == 0:
+        return 1
+    if n == 1:
+        return 2
+    return 1 + sumFib_rec(n - 2) + sumFib_rec(n - 1)
+
+print(sumFib_rec(3))
+print(sumFib_rec(4))
+
+print(sumFib_rec(10))
+print('')
+
+# tuple and list unpacking
+a = [3, 4, 5, 600]
+
+# unpack list to variables
+m1, m2, m3, m4 = a
+print(m4)
+
+m1, m2 = 3, 4  #
+print(m1, m2)
+print('')
+
+# the left-hand side => same elements as needed
+# m1, m2 = a   error too many values to unpack
+
+# unpack
+m1, m2, *r = a
+# in r => elements are stored as a list
+
+# we use a list for "r"
+m1, m2, *r = (1, 2, 3, 4, 5, 6, 7)
+print(r)
+
+m1, m2, *r = 1, 2, 3, 4, 5, 6, 7
+print(r)
+
+print('')
+
+# use: http://interactivepython.org/runestone/static/pythonds/index.html#
+# http://interactivepython.org/runestone/static/pythonds/Recursion/toctree.html
+
+
+
+# https://en.wikipedia.org/wiki/Greatest_common_divisor
+
+# greatest common divisor
+def mkd(a, b):
+    if a == b:
+        return a
+
+    # return mkd(abs(a-b), min(a,b))
+    return mkd(a - b, b) if a > b else mkd(a, b - a)  # x if a>b else y
+
+print(mkd(15, 3))
+print(mkd(90, 12))
+
+print('')
+
+# https://www.w3resource.com/c-programming-exercises/recursion/index.php
+
+# exit maze
+# recursion problem => maze
+
+"""
+maze problem, recursion
+
+The maze is a 2D array of integers.
+The maze has 0 and 1 where 0 means empty and 1 means wall.
+
+The input is 0,0 and the output is given.
+For example, the output can be len(.)-1,len(.)-1.
+
+If I reach the output, then I have finished.
+If not, then I try 4 different options => recursion.
+I try 4 different options if they have 0 (and not 1).
+
+Infinite loop => I should not pass from places I have visited.
+
+I should not go to places I have visited before.
+I mark the places I have visited with 2.
+
+The 2 should mark the correct way in the end. I must delete the 2 if there is no way out.
+"""
+
+array2D = [[0 for i in range(6)] for j in range(6)]
+print(array2D)
+
+print(len(array2D))
+print('')
+
+for i in range(len(array2D)):
+    print(array2D[i])
+    print
+
+array2D[3][4] = 1
+print('')
+
+for i in range(len(array2D)):
+    print(array2D[i])
+    print
+
+import numpy as np
+
+print('')
+
+length1 = 6
+array2D = np.random.randint(2, size=(length1, length1))
+
+array2D[0][0] = 0
+array2D[len(array2D) - 1][len(array2D) - 1] = 0
+
+array2D[1][0] = 0
+array2D[2][0] = 0
+array2D[2][1] = 0
+array2D[2][2] = 0
+array2D[3][2] = 0
+array2D[3][3] = 0
+array2D[3][4] = 0
+array2D[4][4] = 0
+array2D[4][5] = 0
+
+# array2D[len(array2D)-1][len(array2D)-1] = 3
+print(array2D)
+print('')
+
+print((5, 6) == (3, 4))
+print((5, 6) == (5, 6))
+print('')
+
+stack1 = [4]
+print(stack1.pop())
+
+stack1 = [4, 5, 2]
+print(stack1.pop())
+print('')
+
+stack1 = []
+stack2 = []
+
+# stack1.append(0)
+# stack2.append(0)
+
+# recursion, maz problem
+def maze_rec(array2D, endGiven, currentPosition=0, currentPosition2=0, var1=0, var2=0):
+    array2D[currentPosition][currentPosition2] = 2
+
+    # global variables
+    global stack1, stack2
+
+    # print(currentPosition)
+    # print(currentPosition2)
+
+    stack1.append(currentPosition)
+    stack2.append(currentPosition2)
+
+    if (currentPosition, currentPosition2) == endGiven:
+        return True
+
+    # print(array2D)
+
+    if array2D[currentPosition + 1][currentPosition2] == 0 and 0 < currentPosition + 1 < len(array2D) - 1 and (
+    currentPosition + 1, currentPosition2) != (var1, var2):
+        return maze_rec(array2D, endGiven, currentPosition + 1, currentPosition2, currentPosition, currentPosition2)
+    if array2D[currentPosition][currentPosition2 + 1] == 0 and 0 < currentPosition2 + 1 < len(array2D) - 1 and (
+    currentPosition, currentPosition2 + 1) != (var1, var2):
+        return maze_rec(array2D, endGiven, currentPosition, currentPosition2 + 1, currentPosition, currentPosition2)
+    if array2D[currentPosition - 1][currentPosition2] == 0 and 0 < currentPosition - 1 < len(array2D) - 1 and (
+    currentPosition - 1, currentPosition2) != (var1, var2):
+        return maze_rec(array2D, endGiven, currentPosition - 1, currentPosition2, currentPosition, currentPosition2)
+    if array2D[currentPosition][currentPosition2 - 1] == 0 and 0 < currentPosition2 - 1 < len(array2D) - 1 and (
+    currentPosition, currentPosition2 - 1) != (var1, var2):
+        return maze_rec(array2D, endGiven, currentPosition, currentPosition2 - 1, currentPosition, currentPosition2)
+
+    print(array2D)
+
+    print(currentPosition)
+    print(currentPosition2)
+
+    array2D[currentPosition][currentPosition2] = 0
+
+    var1 = stack1.pop()
+    var2 = stack2.pop()
+
+    # print(stack1.pop())
+    # print(stack2.pop())
+
+    # return maze_rec(array2D, endGiven, stack1.pop(), stack2.pop(), var1, var2)
+    return maze_rec(array2D, endGiven, stack1.pop(), stack2.pop(), var1, var2) if len(stack1) > 0 \
+        else False
+
+    # return maze_rec(array2D, endGiven, stack1[len(stack1)-1], stack2[len(stack2)-1])
+    # return maze_rec(array2D, endGiven, stack1.pop(), stack2.pop()) if len(stack1)>0 \
+    # else maze_rec(array2D, endGiven)
+
+    # return maze_rec(array2D, endGiven, stack1.pop(), stack2.pop()) if len(stack1)>0 \
+    # else False
+
+    # array2D[currentPosition][currentPosition2] = 0
+
+#    if array2D[currentPosition+1][currentPosition2]==2 and 0<currentPosition+1<len(array2D)-1:
+#        return maze_rec(array2D, endGiven, currentPosition+1, currentPosition2)
+#    if array2D[currentPosition][currentPosition2+1]==2 and 0<currentPosition2+1<len(array2D)-1:
+#        return maze_rec(array2D, endGiven, currentPosition, currentPosition2+1)
+#    if array2D[currentPosition-1][currentPosition2]==2 and 0<currentPosition-1<len(array2D)-1:
+#        return maze_rec(array2D, endGiven, currentPosition-1, currentPosition2)
+#    if array2D[currentPosition][currentPosition2-1]==2 and 0<currentPosition2-1<len(array2D)-1:
+#        return maze_rec(array2D, endGiven, currentPosition, currentPosition2-1)
+
+# call the function maze_rec
+# maze_rec(array2D, (len(array2D)-1, len(array2D)-1))
+print(maze_rec(array2D, (len(array2D) - 1, len(array2D) - 1)))
+
+print('')
+print(array2D)
 
