cc62d85a59ad2ce09941f422eff645bfc44ef32d
nd1511 <nd1511@imperial.ac.uk>
2019-03-28 13:29:53 +0000

    Python, PyCharm IDE
    Python, PyCharm IDE

--- program3_NN.py
+++ program3_NN.py
@@ -1,11 +1,15 @@
-
+# use numpy
 import numpy as np
+
+# PyTorch
 import torch
 
 from torch.autograd import Variable
 
+# we use matplotlib
 import matplotlib.pyplot as plt
 
+# use pandas
 import pandas as pd
 
 #df = pd.read_csv('cancer_data.csv')
@@ -158,8 +162,6 @@ accuracy = torch.sum(correct)/correct.shape[0]
 
 print(accuracy)
 
-
-
 # we store our parameters
 
 # we use the state dictionary "mynet.state_dict()"
@@ -172,6 +174,434 @@ print(accuracy)
 
 
 
+# use numpy
+import numpy as np
+
+#matplotlib inline
+import matplotlib.pyplot as plt
+
+# use tensorflow
+import tensorflow as tf
+
+# we use the MNIST dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+# https://towardsdatascience.com/image-classification-in-10-minutes-with-mnist-dataset-54c35b77a38d
+# use: https://towardsdatascience.com/image-classification-in-10-minutes-with-mnist-dataset-54c35b77a38d
+
+# use matplotlib
+import matplotlib.pyplot as plt
+
+image_index = 7777
+
+# The label is 8
+print(y_train[image_index])
+plt.imshow(x_train[image_index], cmap='Greys')
+
+#plt.pause(5)
+plt.pause(2)
+
+#x_train.shape
+print(x_train.shape)
+
+# Reshaping the array to 4-dims so that it can work with the Keras API
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+# we define the input shape
+input_shape = (28, 28, 1)
+
+# the values are float so that we can get decimal points after division
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+# Normalizing the RGB codes by dividing it to the max RGB value.
+x_train /= 255
+x_test /= 255
+
+print('x_train shape:', x_train.shape)
+
+print('Number of images in x_train', x_train.shape[0])
+print('Number of images in x_test', x_test.shape[0])
 
 
+
+# Importing the required Keras modules containing model and layers
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+
+# Creating a Sequential Model and adding the layers
+model = Sequential()
+
+model.add(Conv2D(28, kernel_size=(3,3), input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Flatten the 2D arrays for fully connected layers
+model.add(Flatten())
+
+model.add(Dense(128, activation=tf.nn.relu))
+
+model.add(Dropout(0.2))
+model.add(Dense(10,activation=tf.nn.softmax))
+
+# compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# ADAM, adaptive momentum
+# we use the Adam optimizer
+
+# fit the model
+#model.fit(x=x_train,y=y_train, epochs=10)
+
+#model.fit(x=x_train,y=y_train, epochs=10)
+model.fit(x=x_train,y=y_train, epochs=8)
+
+# evaluate the model
+model.evaluate(x_test, y_test)
+
+# https://towardsdatascience.com/image-classification-in-10-minutes-with-mnist-dataset-54c35b77a38d
+
+# use index 4444
+image_index = 4444
+
+plt.imshow(x_test[image_index].reshape(28, 28),cmap='Greys')
+
+#plt.pause(5)
+plt.pause(2)
+
+#pred = model.predict(x_test[image_index].reshape(1, img_rows, img_cols, 1))
+pred = model.predict(x_test[image_index].reshape(1, 28, 28, 1))
+
+print(pred.argmax())
+
+
+
+# Deep Generative Models
+# GANs and VAEs, Generative Models
+
+# random noise
+# from random noise to a tensor
+
+# We use batch normalisation.
+# GANs are very difficult to train. Super-deep models. This is why we use batch normalisation.
+
+# GANs and LSTM RNNs
+# use LSTM RNNs together with GANs
+
+# combine the power of LSTM RNNs and GANs
+# it is possible to use LSTM RNN together with GANs
+
+# https://github.com/life-efficient/Academy-of-AI/blob/master/Lecture%2013%20-%20Generative%20Models/GANs%20tutorial.ipynb
+
+# https://github.com/life-efficient/Academy-of-AI/tree/master/Lecture%2013%20-%20Generative%20Models
+# https://github.com/life-efficient/Academy-of-AI/blob/master/Lecture%2013%20-%20Generative%20Models/GANs%20tutorial.ipynb
+
+
+
+# Anomaly detection (AD)
+# Unsupervised machine learning
+
+# GANs for super-resolution
+# Generative Adversarial Networks, GANs
+
+# the BigGAN dataset
+# BigGAN => massive dataset
+# latent space, BigGAN, GANs
+
+# down-sampling, sub-sample, pooling
+# throw away samples, pooling, max-pooling
+
+# partial derivatives
+# loss function and partial derivatives
+
+# https://github.com/Students-for-AI/The-Academy-of-AI
+# https://github.com/life-efficient/Academy-of-AI/tree/master/Lecture%2013%20-%20Generative%20Models
+
+# Generator G and Discriminator D
+# the loss function of the Generator G
+
+# up-convolution
+# We use a filter we do up-convolution with.
+
+# use batch normalisation
+# GANs are very difficult to train and this is why we use batch normalisation.
+
+# We normalize across a batch.
+# Mean across a batch. We use batches. Normalize across a batch.
+
+# the ReLU activation function
+# ReLU is the most common activation function. We use ReLU.
+
+# use: https://github.com/life-efficient/Academy-of-AI/blob/master/Lecture%2013%20-%20Generative%20Models/GANs%20tutorial.ipynb
+
+
+
+# we use PyTorch
+import torch
+
+#import torch
+import torchvision
+
+from torchvision import datasets, transforms
+
+# use matplotlib
+import matplotlib.pyplot as plt
+
+#import torch
+#import torchvision
+
+#from torchvision import transforms, datasets
+
+# use nn.functional
+import torch.nn.functional as F
+
+#import matplotlib.pyplot as plt
+#batch_size = 128
+
+# download the training dataset
+#train_data = datasets.FashionMNIST(root='fashiondata/',
+#                                   transform=transforms.ToTensor(),
+#                                   train=True,
+#                                   download=True)
+
+# we create the train data loader
+#train_loader = torch.utils.data.DataLoader(train_data,
+#                                           shuffle=True,
+#                                           batch_size=batch_size)
+
+# define the batch size
+batch_size = 100
+
+train_data = datasets.FashionMNIST(root='fashiondata/',
+                                 transform=transforms.ToTensor(),
+                                 train=True,
+                                 download=True
+                                 )
+
+train_samples = torch.utils.data.DataLoader(dataset=train_data,
+                                           batch_size=batch_size,
+                                           shuffle=True
+                                           )
+
+# combine the power of LSTM RNNs and GANs
+# it is possible to use LSTM RNN together with GANs
+
+# GANs and LSTM RNNs
+# use LSTM RNNs together with GANs
+
+# class for D and G
+# we train the discriminator and the generator
+
+# we make the discriminator
+class discriminator(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = torch.nn.Conv2d(1, 64, kernel_size=4, stride=2, padding=1)  # 1x28x28-> 64x14x14
+        self.conv2 = torch.nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)  # 64x14x14-> 128x7x7
+
+        self.dense1 = torch.nn.Linear(128 * 7 * 7, 1)
+
+        self.bn1 = torch.nn.BatchNorm2d(64)
+        self.bn2 = torch.nn.BatchNorm2d(128)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x))).view(-1, 128 * 7 * 7)
+
+        # use sigmoid for the output layer
+        x = F.sigmoid(self.dense1(x))
+
+        return x
+
+# this was for the discriminator
+# we now do the same for the generator
+
+# Generator G
+class generator(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.dense1 = torch.nn.Linear(128, 256)
+        self.dense2 = torch.nn.Linear(256, 1024)
+        self.dense3 = torch.nn.Linear(1024, 128 * 7 * 7)
+
+        self.uconv1 = torch.nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)  # 128x7x7 -> 64x14x14
+        self.uconv2 = torch.nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1)  # 64x14x14 -> 1x28x28
+
+        self.bn1 = torch.nn.BatchNorm1d(256)
+        self.bn2 = torch.nn.BatchNorm1d(1024)
+        self.bn3 = torch.nn.BatchNorm1d(128 * 7 * 7)
+        self.bn4 = torch.nn.BatchNorm2d(64)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.dense1(x)))
+        x = F.relu(self.bn2(self.dense2(x)))
+        x = F.relu(self.bn3(self.dense3(x))).view(-1, 128, 7, 7)
+
+        x = F.relu(self.bn4(self.uconv1(x)))
+
+        x = F.sigmoid(self.uconv2(x))
+        return x
+
+# https://github.com/life-efficient/Academy-of-AI/blob/master/Lecture%2013%20-%20Generative%20Models/GANs%20tutorial.ipynb
+# use: https://github.com/life-efficient/Academy-of-AI/blob/master/Lecture%2013%20-%20Generative%20Models/GANs%20tutorial.ipynb
+
+# instantiate the model
+d = discriminator()
+g = generator()
+
+# training hyperparameters
+#epochs = 100
+
+#epochs = 100
+epochs = 10
+
+# learning rate
+#dlr = 0.0003
+#glr = 0.0003
+
+dlr = 0.003
+glr = 0.003
+
+d_optimizer = torch.optim.Adam(d.parameters(), lr=dlr)
+g_optimizer = torch.optim.Adam(g.parameters(), lr=glr)
+
+dcosts = []
+gcosts = []
+
+plt.ion()
+fig = plt.figure()
+
+loss_ax = fig.add_subplot(121)
+loss_ax.set_xlabel('Batch')
+
+loss_ax.set_ylabel('Cost')
+loss_ax.set_ylim(0, 0.2)
+
+generated_img = fig.add_subplot(122)
+
+plt.show()
+
+# https://github.com/life-efficient/Academy-of-AI/blob/master/Lecture%2013%20-%20Generative%20Models/GANs%20tutorial.ipynb
+
+# https://github.com/life-efficient/Academy-of-AI/tree/master/Lecture%2013%20-%20Generative%20Models
+# use: https://github.com/life-efficient/Academy-of-AI/blob/master/Lecture%2013%20-%20Generative%20Models/GANs%20tutorial.ipynb
+
+def train(epochs, glr, dlr):
+    g_losses = []
+    d_losses = []
+
+    for epoch in range(epochs):
+
+        # iteratre over mini-batches
+        for batch_idx, (real_images, _) in enumerate(train_samples):
+
+            z = torch.randn(batch_size, 128)  # generate random latent variable to generate images from
+            generated_images = g.forward(z)  # generate images
+
+            gen_pred = d.forward(generated_images)  # prediction of discriminator on generated batch
+            real_pred = d.forward(real_images)  # prediction of discriminator on real batch
+
+            dcost = -torch.sum(torch.log(real_pred)) - torch.sum(torch.log(1 - gen_pred))  # cost of discriminator
+            gcost = -torch.sum(torch.log(gen_pred)) / batch_size  # cost of generator
+
+            # train discriminator
+            d_optimizer.zero_grad()
+            dcost.backward(retain_graph=True)  # retain the computational graph so we can train generator after
+            d_optimizer.step()
+
+            # train generator
+            g_optimizer.zero_grad()
+
+            gcost.backward()
+            g_optimizer.step()
+
+            # give us an example of a generated image after every 10000 images produced
+            #if batch_idx * batch_size % 10000 == 0:
+
+            # give us an example of a generated image after every 20 images produced
+            if batch_idx % 20 == 0:
+                g.eval()  # put in evaluation mode
+                noise_input = torch.randn(1, 128)
+                generated_image = g.forward(noise_input)
+
+                generated_img.imshow(generated_image.detach().squeeze(), cmap='gray_r')
+
+                # pause for some seconds
+                plt.pause(5)
+
+                # put back into training mode
+                g.train()
+
+            dcost /= batch_size
+            gcost /= batch_size
+
+            print('Epoch: ', epoch, 'Batch idx:', batch_idx, '\tDisciminator cost: ', dcost.item(),
+                  '\tGenerator cost: ', gcost.item())
+
+            dcosts.append(dcost)
+            gcosts.append(gcost)
+
+            loss_ax.plot(dcosts, 'b')
+            loss_ax.plot(gcosts, 'r')
+
+            fig.canvas.draw()
+
+#print(torch.__version__)
+train(epochs, glr, dlr)
+
+# We obtain:
+# Epoch:  0 Batch idx: 0 	Disciminator cost:  1.3832124471664429 	Generator cost:  0.006555716972798109
+# Epoch:  0 Batch idx: 1 	Disciminator cost:  1.0811840295791626 	Generator cost:  0.008780254982411861
+# Epoch:  0 Batch idx: 2 	Disciminator cost:  0.8481155633926392 	Generator cost:  0.011281056329607964
+# Epoch:  0 Batch idx: 3 	Disciminator cost:  0.6556042432785034 	Generator cost:  0.013879001140594482
+# Epoch:  0 Batch idx: 4 	Disciminator cost:  0.5069876909255981 	Generator cost:  0.016225570812821388
+# Epoch:  0 Batch idx: 5 	Disciminator cost:  0.4130948781967163 	Generator cost:  0.018286770209670067
+# Epoch:  0 Batch idx: 6 	Disciminator cost:  0.33445805311203003 	Generator cost:  0.02015063539147377
+# Epoch:  0 Batch idx: 7 	Disciminator cost:  0.279323011636734 	Generator cost:  0.021849267184734344
+# Epoch:  0 Batch idx: 8 	Disciminator cost:  0.2245958000421524 	Generator cost:  0.02352861315011978
+# Epoch:  0 Batch idx: 9 	Disciminator cost:  0.18664218485355377 	Generator cost:  0.025215130299329758
+# Epoch:  0 Batch idx: 10 	Disciminator cost:  0.14700829982757568 	Generator cost:  0.02692217379808426
+
+# Epoch:  0 Batch idx: 32 	Disciminator cost:  0.2818330228328705 	Generator cost:  0.022729918360710144
+# Epoch:  0 Batch idx: 33 	Disciminator cost:  0.7310256361961365 	Generator cost:  0.05649786815047264
+# Epoch:  0 Batch idx: 34 	Disciminator cost:  0.31759023666381836 	Generator cost:  0.02075548656284809
+# Epoch:  0 Batch idx: 35 	Disciminator cost:  0.35554683208465576 	Generator cost:  0.018939709290862083
+# Epoch:  0 Batch idx: 36 	Disciminator cost:  0.07700302451848984 	Generator cost:  0.04144695773720741
+# Epoch:  0 Batch idx: 37 	Disciminator cost:  0.08900360018014908 	Generator cost:  0.05888563022017479
+# Epoch:  0 Batch idx: 38 	Disciminator cost:  0.0921328067779541 	Generator cost:  0.0593753345310688
+# Epoch:  0 Batch idx: 39 	Disciminator cost:  0.09943853318691254 	Generator cost:  0.05279992148280144
+# Epoch:  0 Batch idx: 40 	Disciminator cost:  0.2455407679080963 	Generator cost:  0.036564696580171585
+# Epoch:  0 Batch idx: 41 	Disciminator cost:  0.10074597597122192 	Generator cost:  0.03721988573670387
+# Epoch:  0 Batch idx: 42 	Disciminator cost:  0.07906078547239304 	Generator cost:  0.04363853484392166
+
+# Epoch:  0 Batch idx: 108 	Disciminator cost:  0.22247043251991272 	Generator cost:  0.03322262689471245
+# Epoch:  0 Batch idx: 109 	Disciminator cost:  0.20719386637210846 	Generator cost:  0.02638845518231392
+# Epoch:  0 Batch idx: 110 	Disciminator cost:  0.2795112133026123 	Generator cost:  0.027195550501346588
+# Epoch:  0 Batch idx: 111 	Disciminator cost:  0.49694764614105225 	Generator cost:  0.02403220161795616
+# Epoch:  0 Batch idx: 112 	Disciminator cost:  0.581132173538208 	Generator cost:  0.026757290586829185
+# Epoch:  0 Batch idx: 113 	Disciminator cost:  0.16659873723983765 	Generator cost:  0.0335114412009716
+# Epoch:  0 Batch idx: 114 	Disciminator cost:  0.0639999508857727 	Generator cost:  0.04211951419711113
+# Epoch:  0 Batch idx: 115 	Disciminator cost:  0.018385086208581924 	Generator cost:  0.05511172115802765
+# Epoch:  0 Batch idx: 116 	Disciminator cost:  0.012170110829174519 	Generator cost:  0.06555930525064468
+# Epoch:  0 Batch idx: 117 	Disciminator cost:  0.006641524378210306 	Generator cost:  0.07086272537708282
+# Epoch:  0 Batch idx: 118 	Disciminator cost:  0.010556117631494999 	Generator cost:  0.06929603219032288
+# Epoch:  0 Batch idx: 119 	Disciminator cost:  0.017774969339370728 	Generator cost:  0.07270769774913788
+
+# Epoch:  0 Batch idx: 444 	Disciminator cost:  0.06787727028131485 	Generator cost:  0.04046594724059105
+# Epoch:  0 Batch idx: 445 	Disciminator cost:  0.07139576226472855 	Generator cost:  0.03837932273745537
+# Epoch:  0 Batch idx: 446 	Disciminator cost:  0.08202749490737915 	Generator cost:  0.039551254361867905
+# Epoch:  0 Batch idx: 447 	Disciminator cost:  0.12328958511352539 	Generator cost:  0.03817861154675484
+# Epoch:  0 Batch idx: 448 	Disciminator cost:  0.06865841150283813 	Generator cost:  0.03938257694244385
+
+# generate random latent variable to generate images
+z = torch.randn(batch_size, 128)
+
+# generate images
+im = g.forward(z)
+# use "forward(.)"
+
+plt.imshow(im)
 
